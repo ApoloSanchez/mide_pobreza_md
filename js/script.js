367cfb06f223d87119ad25e746ff1016ca4a0476
Apolo Sánchez <apolo.comun@gmail.com>
2022-10-22 15:28:42 -0500

    Agregados scripts para ingreso de nombre y año de nacimiento

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert ("¡Hola!, este simulador está diseñado para medir nivel de pobreza en hogares unipersonales.")
+
+let nombre = prompt ("Ingresa tu nombre");
+while (nombre == "" ){
+    nombre= prompt ("Debes ingresar tu nombre");
+}
+
+let birth = prompt ("Ingresa el año en que naciste en cuatro dígitos; por ejemplo: 1986");
+while (birth == "" ){
+    birth= prompt ("Debes ingresar el año en que naciste en cuatro dígitos; por ejemplo: 1986");
+}
