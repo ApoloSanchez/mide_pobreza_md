ccff155d1120456cd405effb46cb7e22ff63aaf3
Apolo Sánchez <apolo.comun@gmail.com>
2022-10-22 21:58:45 -0500

    Definidos los elementos del objeto Usuario

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 alert ("¡Hola!, este simulador está diseñado para medir nivel de pobreza en hogares unipersonales.")
 
-let nombre = prompt ("Ingresa tu nombre");
-while (nombre == "" ){
-    nombre= prompt ("Debes ingresar tu nombre");
-}
+// let nombre = prompt ("Folio identificador de persona a evaluar");
+// while (nombre == "" ){
+//     nombre= prompt ("Es necesario ingresar un folio identificador de la persona a evaluar");
+// }
 
-let birth = prompt ("Ingresa el año en que naciste en cuatro dígitos; por ejemplo: 1986");
-while (birth == "" ){
-    birth= prompt ("Debes ingresar el año en que naciste en cuatro dígitos; por ejemplo: 1986");
+// let birth = parseInt (prompt ("Ingresa el año de nacimiento"));
+// while ((birth == "") || (birth <1922) || (birth>2012)){
+//     birth= parseInt (prompt ("Es necesario ingresar el año de nacimiento correcto"));
+// }
+
+function Usuario(folio, cumpleanios, ingreso, estudios, salud, alimentacion, vivienda, servicios,cohesion,carretera) {
+    this.folio=folio;
+    this.cumpleanios=cumpleanios;
+    this.ingreso=ingreso;
+    this.estudios=estudios;
+    this.salud=salud;
+    this.alimentacion=alimentacion;
+    this.vivienda=vivienda;
+    this.servicios=servicios;
+    this.cohesion=cohesion;
+    this.carretera=carretera;
 }
+let folio=prompt("INGRESE EL FOLIO IDENTIFICADOR DE LA PERSONA A EVALUAR");
+let cumpleanios=prompt("INGRESE EÑ AÑO DE NACIMIENTO");
+let ingreso=prompt("xx");
+let estudios=prompt("INGRESE SU ÚLTIMO GRADO DE ESTUDIOS");
+let salud=prompt("¿CUENTA CON SEGURIDAD SOCIAL? \n en caso afirmativo, escriba 1 \n en caso negativo, escriba 0");
+let alimentacion=prompt("xx");
+let vivienda=prompt("xx");
+let servicios=prompt("xx");
+let cohesion=prompt("xx");
+let carretera=prompt("xx");
+
+
+const Usuario1=new Usuario(folio, cumpleanios, ingreso, estudios, salud, alimentacion, vivienda, servicios,cohesion,carretera);
+console.log(Usuario1.folio);
